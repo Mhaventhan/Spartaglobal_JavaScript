04b62999789e8513a33ed92d88e67a3a5b66ef1f
“Mhaventhan” <mhaventhan@gmail.com>
2018-08-07 10:39:41 +0100

    Comments have been added

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,22 +1,23 @@
-var number_1 = prompt("Please enter first number");
+var number_1 = parseFloat(prompt("Please enter first number"));
+// operator for the Calculator
 var op = prompt("Please enter operator");
-var number_2 = prompt("Please enter second number");
-console.log(number_1);
-console.log(op);
-console.log(number_2);
+var number_2 = parseFloat(prompt("Please enter second number"));
 
-a = parseInt(number_1) ;
-b = parseInt(number_2);
-if (op == "+") { number_1 + number_2
-  console.log(a + b);
+
+
+if (op == "+")
+{
+  console.log( number_1 + number_2);
   }
-  
-if (op == "-") { number_1 - number_2
-  console.log(a - b);
+
+if (op == "-")
+{
+  console.log( number_1 - number_2);
 }
-if (op == "*") { number_1 * number_2
-  console.log(a * b);
+if (op == "*")
+{
+  console.log(number_1 * number_2);
 }
-if (op == "/") { number_1 / number_2
-  console.log(a / b);
+if (op == "/") {
+  console.log(number_1 / number_2);
 }
