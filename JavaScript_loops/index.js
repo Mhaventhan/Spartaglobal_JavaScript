47234d97086e1dd4af592c01e010a4bcf2b705db
“Mhaventhan” <mhaventhan@gmail.com>
2018-08-08 21:45:35 +0100

    added read me file

---
+++ JavaScript_loops/index.js
@@ -0,0 +1,37 @@
+// For Loops
+    // starting point, how many loops, increment after each loop
+// for(var i=0; i < 10; i++){
+//   console.log("this is loop" + i);
+// }
+
+// var my_array = ["Richard","Is a", "Golfer"];
+// for (var i = 0; i < my_array.length; i++) {
+//   console.log(my_array[i]);
+// }
+
+//  while loop
+// var i = 0;
+// while ( i < 5) {
+//   console.log("this loop has run" + i + "times");
+// i++;
+// }
+
+// do while
+
+// var number;
+// do {
+//   number = Math.random()
+//   console.log(number);
+// }while (number < 0.5);
+
+// for in loops
+// var my_object = new Object();
+// my_object.a = "1";
+// my_object.b = "2";
+// my_object.c = "3";
+//
+// debugger;
+// for(var key in my_object){
+//   console.log(key);
+//   console.log(my_object[key]);
+// }
